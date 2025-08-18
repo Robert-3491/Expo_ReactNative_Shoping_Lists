69f6c662272941a97986b53b0d4a3d17fb03bbf0
rubynzar <rubynzar@yahoo.com>
2025-08-18 11:55:44 +0200

    Import page work

--- components/ImportContentPage/importContentWrapper.tsx
+++ components/ImportContentPage/importContentWrapper.tsx
@@ -1,41 +1,77 @@
-import { StyleSheet, View } from "react-native";
-import React from "react";
+import {
+  Keyboard,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import Header from "../SharedComponents/header";
 import TextDefault from "../SharedComponents/textDefault";
 import CustomButton from "../SharedComponents/customButton";
 import { colors } from "@/assets/colors";
-import { getClipboardText } from "@/containers/importContentContainer";
+import {
+  getClipboardText,
+  importContentController,
+} from "@/containers/importContentContainer";
 import ImportInput from "./importInput";
-import { useClipboard } from "@react-native-clipboard/clipboard";
 
 const ImportContentWrapper = () => {
-  const [inputText, setInputText] = useClipboard(); // it's actualy the fucking clipboard, setInputText changes the fucking clipboard..wow
+  const [inputText, setInputText] = useState("");
+
+  useEffect(() => {
+    const loadClipboard = async () => {
+      setInputText(await getClipboardText());
+    };
+    loadClipboard();
+  }, []);
 
   return (
-    <View style={{ flex: 1 }}>
-      <Header>Import content</Header>
-      <View style={{ paddingHorizontal: 15 }}>
-        <TextDefault
-          color={colors.textSecondary}
-          margin={{ marginVertical: 10 }}
-        >
-          Press the Paste button to replace the input content with your
-          clipboard content.{`\n`}Long press to add. Multiple lists are allowed.
-        </TextDefault>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Header>Import content</Header>
+        <View style={styles.content}>
+          <TextDefault
+            color={colors.textSecondary}
+            margin={{ marginVertical: 10 }}
+          >
+            Press the Paste button to replace the input content with your
+            clipboard content.{`\n`}Long press to add. Multiple lists are
+            allowed.
+          </TextDefault>
 
-        <CustomButton
-          buttonText="Paste from Clipboard"
-          fontSize={18}
-          backgroundColor={colors.primaryLight}
-          onPress={async () => setInputText(await getClipboardText())}
-        ></CustomButton>
+          <CustomButton
+            buttonText="Paste from Clipboard"
+            fontSize={18}
+            backgroundColor={colors.primaryLight}
+            onPress={async () => setInputText(await getClipboardText())}
+            onLongPress={async () =>
+              setInputText(`${inputText}\n${await getClipboardText()}`)
+            }
+          />
 
-        <ImportInput inputText={inputText} setInputText={setInputText} />
+          <ImportInput inputText={inputText} setInputText={setInputText} />
+
+          <CustomButton
+            buttonText="Import"
+            fontSize={18}
+            backgroundColor={colors.successToast}
+            onPress={() => importContentController(inputText)}
+          />
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
 export default ImportContentWrapper;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    paddingHorizontal: 15,
+    paddingBottom: 5,
+    flex: 1,
+  },
+});
